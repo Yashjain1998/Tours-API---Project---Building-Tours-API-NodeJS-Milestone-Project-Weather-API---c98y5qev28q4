8ce3ea27603c3371483b53ebab8991abc5efe010
Yash jain <126048815+Yashjain1998@users.noreply.github.com>
2023-08-07 22:09:18 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const tourDetails = JSON.parse(fs.readFileSync(`${__dirname}/data/tours.json`));
 
 app.get('/tours', (req, res) => {
   //write a code here to get all the tours from tours.json
+  res.status(200).json(JSON.stringify(tourDetails));
 });
 
 app.post('/tours', (req, res) => {
@@ -16,6 +17,17 @@ app.post('/tours', (req, res) => {
   //Write a code here for creating new tour from data/tours.json
   //For creating new id use this logic
   // const newId = tourDetails[tourDetails.length - 1].id + 1;
+  newtour={
+    id: tourDetails[tourDetails.length - 1].id + 1,
+    name,
+    description,
+    duration,
+    price,
+  }
+  tourDetails.push(newtour);
+  fs.writeFileSync(`${__dirname}/data/tours.json`,tourDetails);
+
+  res.status(200).json({"message": "Tour added successfully"})
 });
 
 app.put('/tours/:id', (req, res) => {
@@ -23,11 +35,22 @@ app.put('/tours/:id', (req, res) => {
   const updatedTour = req.body;
 
   //write a code here for updating a tour
+  const index=tourDetails.findindex(({id})=>id==tourId);
+  tourDetails[index]=updatedTour;
+  fs.writeFileSync(`${__dirname}/data/tours.json`,tourDetails);
+
+  res.status(200).json({"message": "Tour updated successfully"});
 });
 
 app.delete('/tours/:id', (req, res) => {
   const tourId = parseInt(req.params.id);
   //Write a code here for deleting a tour from data/tours.json
+  const index=tourDetails.findindex(({id})=>id==tourId);
+  tourDetails.splice(index,1);
+  fs.writeFileSync(`${__dirname}/data/tours.json`,tourDetails);
+
+  res.status(200).json({"message": "Tour deleted successfully"});
+
 });
 
 module.exports = app;
